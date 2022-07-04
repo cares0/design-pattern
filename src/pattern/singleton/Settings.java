60f24cbb5fb4af958924320657982cccbc259ab6
cares0 <cares00000@gmail.com>
2022-07-04 17:10:53 +0900

    singleton4 - double checked locking 적용

--- src/pattern/singleton/Settings.java
+++ src/pattern/singleton/Settings.java
@@ -2,13 +2,20 @@ package pattern.singleton;
 
 public class Settings {
 
-    private static final Settings INSTANCE = new Settings();
+    private static volatile Settings instance;
 
     private Settings() {
 
     }
 
     public static Settings getInstance() {
-        return INSTANCE;
+        if (instance == null) {
+            synchronized (Settings.class) {
+                if (instance == null) {
+                    instance = new Settings();
+                }
+            }
+        }
+        return instance;
     }
 }
